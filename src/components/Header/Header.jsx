ffdb6696429412de25df709f331c8997a3fe05b0
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-30 00:09:33 +0600

    nested link added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,11 @@ import "./Header.css"
 const Header = () => {
     return (
         <div>
-            <h2>Navbar</h2>
             <nav>
+                <span>My Website</span>
+                <br />
                 <Link to="/">Home</Link>
+                <Link to="/users">Users</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact Us</Link>
                 
