2acc5b91ebf7e4c2ba583283cedf750d4b1bfa8a
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-29 22:51:13 +0600

    nested route started

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 const router = new createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello React Router</div>
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
   },
-  {
-    path: "/about",
-    element: <div>I am in the about page</div>,
-  },
-  {
-    path: "/contact",
-    element: <div>Call me Now</div>,
-  },
-])
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
