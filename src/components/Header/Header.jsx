8b47c3f102e20fdbcbb70817d243595233b2bc72
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-30 18:15:06 +0600

    useNsvigation, useLocation, NavLink used

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
@@ -8,9 +8,9 @@ const Header = () => {
         <span>My Website</span>
         <br />
         <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-        <Link to={`/posts`}>Posts</Link>
-        <Link to="/about">About</Link>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to={`/posts`}>Posts</NavLink>
+        <NavLink to="/about">About</NavLink>
         <Link to="/contact">Contact Us</Link>
         <Link to={`/comments`}>Comments</Link>
       </nav>
